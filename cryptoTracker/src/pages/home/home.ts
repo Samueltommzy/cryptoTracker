e9a74c2d9e4ef79e10bd144f12c2998773da7885
Samuel <samueloluwatommzy@gmail.com>
2018-01-30 12:56:18 +0100

    Home Page

--- cryptoTracker/src/pages/home/home.ts
+++ cryptoTracker/src/pages/home/home.ts
@@ -1,14 +1,26 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
-
+import { Component }        from '@angular/core';
+import { NavController , IonicPage}    from 'ionic-angular';
+import { HoldingsProvider } from '../../providers/holdings/holdings' 
+@IonicPage()
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(private navCtrl: NavController, private holdingsProvider : HoldingsProvider) {
 
   }
-
+ionViewDidLoad(): void{
+  this.holdingsProvider.getHoldings();
+}
+createHolding(): void{
+  this.navCtrl.push('NewCryptoCoinPage');
+}
+gotoCryptonator(): void{
+ window.open('https://www.cryptonator.com/api', '_system');
+}
+refreshPrices(refresher):void{
+  this.holdingsProvider.fetchPrice(refresher);
+}
 }
