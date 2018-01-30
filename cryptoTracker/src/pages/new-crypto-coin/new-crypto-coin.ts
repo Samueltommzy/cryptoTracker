c2353356c1a8520d2ea05f5c1f8353e132c3a2ff
Samuel <samueloluwatommzy@gmail.com>
2018-01-30 12:55:43 +0100

    Adding a new cryptocoin

--- cryptoTracker/src/pages/new-crypto-coin/new-crypto-coin.ts
+++ cryptoTracker/src/pages/new-crypto-coin/new-crypto-coin.ts
@@ -1,25 +1,46 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
+import { IonicPage, NavController } from 'ionic-angular';
+import { HoldingsProvider } from '../../providers/holdings/holdings'
 /**
  * Generated class for the NewCryptoCoinPage page.
  *
  * See https://ionicframework.com/docs/components/#navigation for more info on
  * Ionic pages and navigation.
  */
-
-@IonicPage()
+@IonicPage({
+defaultHistory :['HomePage']
+})
 @Component({
   selector: 'page-new-crypto-coin',
   templateUrl: 'new-crypto-coin.html',
 })
 export class NewCryptoCoinPage {
-
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  private cryptoUnavailable: boolean = false;
+  private checkingValidity: boolean = false;
+  private cryptoCode: string;
+  private displayCurrency : string;
+  private amountHolding;
+  constructor(private navCtrl: NavController, private holdingsProvider : HoldingsProvider) {
   }
-
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad NewCryptoCoinPage');
-  }
-
+ createHolding(): void{
+   this.cryptoUnavailable = false;
+   this.checkingValidity = true;
+   let holding ={
+     crypto: this.cryptoCode,
+     currency: this.displayCurrency,
+     amount : this.amountHolding || 0
+   };
+   this.holdingsProvider.verify(holding).subscribe((result)=>{
+     this.checkingValidity = false;
+     if(result.success){
+       this.holdingsProvider.createHolding(holding);
+       this.navCtrl.pop();
+     }
+     else{
+       this.cryptoUnavailable = true;
+     }
+   },(err) =>{
+     this.checkingValidity = false;
+   })
+ }
 }
